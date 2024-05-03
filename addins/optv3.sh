f6b24a9ca0419151b15d6de58c5ebd387906993f
Mickel Edward <o2tongpethuk@gmail.com>
2024-05-03 19:00:16 +0700

    remove -mtune from CXXFLAGS

--- addins/optv3.sh
+++ addins/optv3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC2016
 FF_CONFIGURE+=" --cpu=x86-64-v3 --optflags=-O3"
 
 ffbuild_dockeraddin() {
-    to_df 'ENV CFLAGS="$CFLAGS -march=x86-64-v3 -mtune=znver2 -O3" CXXFLAGS="$CXXFLAGS -mtune=znver2 -O3" RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=x86-64-v3 -Clink-arg=-z -Clink-arg=pack-relative-relocs -Ccodegen-units=1" GOAMD64=v3'
+    to_df 'ENV CFLAGS="$CFLAGS -march=x86-64-v3 -mtune=znver2 -O3" CXXFLAGS="$CXXFLAGS -O3" RUSTFLAGS="-Copt-level=3 -Ctarget-cpu=x86-64-v3 -Clink-arg=-z -Clink-arg=pack-relative-relocs -Ccodegen-units=1" GOAMD64=v3'
 }
