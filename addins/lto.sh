6e16e6fc2801b6a2079563f8760695fe31140fac
unhealme <o2tongpethuk@gmail.com>
2024-12-30 14:39:24 +0700

    testimoni

--- addins/lto.sh
+++ addins/lto.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC2016
 FF_CONFIGURE="$FF_CONFIGURE --enable-lto"
 
 ffbuild_dockeraddin() {
-    to_df 'ENV CFLAGS="$CFLAGS -flto=auto" CXXFLAGS="$CXXFLAGS -flto=auto" LDFLAGS="$LDFLAGS -flto=auto"'
+    to_df 'ENV CFLAGS="${CFLAGS} -flto=auto -falign-functions=32" CXXFLAGS="${CXXFLAGS} -flto=auto -falign-functions=32" LDFLAGS="${LDFLAGS} -flto=auto -falign-functions=32"'
     to_df 'ENV FFBUILD_TARGET_FLAGS="$FFBUILD_TARGET_FLAGS --ar=${FFBUILD_TOOLCHAIN}-gcc-ar --nm=${FFBUILD_TOOLCHAIN}-gcc-nm --ranlib=${FFBUILD_TOOLCHAIN}-gcc-ranlib"'
+    to_df 'ENV CARGO_PROFILE_RELEASE_LTO=fat'
 }
