860412a5317c282244a16be576473f4787fe6cc2
unhealme <o2tongpethuk@gmail.com>
2024-12-31 20:14:25 +0700

    debug optv3 + lto

--- addins/optv3.sh
+++ addins/optv3.sh
@@ -6,9 +6,8 @@ FF_CXXFLAGS+=" -march=x86-64-v3 -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_S
 FF_LDFLAGS+=" -Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"
 
 ffbuild_dockeraddin() {
-    to_df 'ENV CFLAGS="${CFLAGS} -march=x86-64-v3 -O3"'
-    to_df 'ENV CXXFLAGS="${CXXFLAGS} -march=x86-64-v3 -O3"'
-    to_df 'ENV LDFLAGS="${LDFLAGS} -Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"'
+    to_df 'ENV CFLAGS="${CFLAGS} -march=x86-64-v3"'
+    to_df 'ENV CXXFLAGS="${CXXFLAGS} -march=x86-64-v3"'
     to_df 'ENV RUSTFLAGS="${RUSTFLAGS} -Copt-level=3 -Ctarget-cpu=x86-64-v3 -Clink-arg=-z -Clink-arg=pack-relative-relocs -Ccodegen-units=1"'
     to_df 'ENV GOAMD64=v3'
 }
