e8be4cd4b17295cdb1827daa5feedfaf3d83ecf8
unhealme <o2tongpethuk@gmail.com>
2025-01-01 23:29:19 +0700

    another test

--- addins/optv3.sh
+++ addins/optv3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # shellcheck disable=SC2016
 FF_CONFIGURE+=" --cpu=x86-64-v3 --optflags=-O3"
-FF_CFLAGS+=" -march=x86-64-v3 -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -mpclmul"
-FF_CXXFLAGS+=" -march=x86-64-v3 -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -mpclmul -Wp,-D_GLIBCXX_ASSERTIONS"
-FF_LDFLAGS+=" -Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"
+#FF_CFLAGS+=" -march=x86-64-v3 -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -mpclmul"
+#FF_CXXFLAGS+=" -march=x86-64-v3 -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -mpclmul -Wp,-D_GLIBCXX_ASSERTIONS"
+#FF_LDFLAGS+=" -Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,-z,pack-relative-relocs"
 
 ffbuild_dockeraddin() {
     to_df 'ENV CFLAGS="${CFLAGS} -march=x86-64-v3"'
